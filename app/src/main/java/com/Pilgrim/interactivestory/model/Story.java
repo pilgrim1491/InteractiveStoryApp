00fa29f04f9b3b7e0ec183a99561b1a966d77ec2
pilgrim1491 <73177704+pilgrim1491@users.noreply.github.com>
2022-06-15 19:19:23 -0400

    First Commit

---
+++ app/src/main/java/com/Pilgrim/interactivestory/model/Story.java
@@ -0,0 +1,21 @@
+package com.Pilgrim.interactivestory.model;
+
+import com.Pilgrim.interactivestory.R;
+
+public class Story {
+    private Page[] pages;
+
+    public Story() {
+        pages = new Page[7];
+
+        pages[0] = new Page(R.drawable.page0, R.string.page0, new Choice(R.string.page0_choice1, 1), new Choice(R.string.page0_choice2, 2));
+
+    }
+
+    public Page getPage(int pageNumber) {
+        if (pageNumber >= pages.length) {
+            pageNumber = 0;
+        }
+        return pages[pageNumber];
+    }
+}
